3633fb769c4194a33ffc1d253e301c422fd7dcc1
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-06-25 13:22:28 -0700

    Test code to connect to localhost

--- src/client.cpp
+++ src/client.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 
 int main () {
-  std::cout << "check\n";
+  ssh::Session session;
+  session.setOption (SSH_OPTIONS_HOST, "localhost");
+  int port = 22;
+  session.setOption (SSH_OPTIONS_PORT, &port);
+  session.setOption (SSH_OPTIONS_USER, "abhi");
+  session.connect ();
+  if (session.getOpensshVersion () == SSH_INT_VERSION(6,6,1)) {
+    std::cout << "version match" << std::endl;
+  } else {
+    std::cout << "version mismatch" << std::endl; 
+  }
+
   return 0;
 }
