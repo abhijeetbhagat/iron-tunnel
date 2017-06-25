fd6176c291acc801cdfd46a00a7a509c32a89324
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-06-25 15:29:25 -0700

    Check server authenticity after connecting

--- src/client.cpp
+++ src/client.cpp
@@ -7,11 +7,26 @@ int main () {
   int port = 22;
   session.setOption (SSH_OPTIONS_PORT, &port);
   session.setOption (SSH_OPTIONS_USER, "abhi");
-  session.connect ();
-  if (session.getOpensshVersion () == SSH_INT_VERSION(6,6,1)) {
+  try {
+    session.connect ();
+  } catch (ssh::SshException & e) {
+    return -1;
+  }
+
+  if (session.getOpensshVersion () == SSH_VERSION_INT (6,6,0)) {
     std::cout << "version match" << std::endl;
   } else {
     std::cout << "version mismatch" << std::endl; 
+  }
+
+  //this is a mandatory step to check the authenticity of the server
+  switch (session.isServerKnown ()) {
+    case SSH_SERVER_KNOWN_OK:
+      std::cout << "known server\n";
+      break;
+    default:
+      std::cout << "unknown server\n"; 
+      return -1;
   }
 
   return 0;
